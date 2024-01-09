2b56dd8016c2ee31c90df53863ba31624c157e6a
Tyler O'Neil <tyleroneildev@gmail.com>
2024-01-09 17:56:35 -0500

    Add OpenAI Connection

--- GPTerminal.py
+++ GPTerminal.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 import sys
+import os
+from dotenv import load_dotenv
 from openai import OpenAI
-client = OpenAI()
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+MODEL = os.getenv("MODEL")
+
+client = OpenAI(
+  api_key=API_KEY
+)
 
 def main():
     if len(sys.argv) > 1:
-        print("testing")
+        response = client.chat.completions.create(
+            model=MODEL,
+            messages=[{"role": "user", "content": sys.argv[1]}],
+        )
+        print(response.choices[0].message.content)
+        
     else:
         print("No input provided.")
 
