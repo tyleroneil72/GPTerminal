e2c7e555756bef0a3f6280538b49dc3bb645fa83
Tyler O'Neil <tyleroneildev@gmail.com>
2024-01-09 21:52:17 -0500

    Remove unused colours

--- GPTerminal.py
+++ GPTerminal.py
@@ -10,14 +10,11 @@ MODEL = "gpt-3.5-turbo"
 
 # ANSI escape sequences for terminal colours
 class Colours:
-    HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 # Setup argument parser for command line interface
 description = """
