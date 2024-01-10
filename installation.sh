380f987281277b67e5985ad7e6c565b0d394e5d4
Tyler O'Neil <tyleroneildev@gmail.com>
2024-01-09 22:44:19 -0500

    Only display Art when terminal is wide enough

--- installation.sh
+++ installation.sh
@@ -9,16 +9,18 @@ chmod +x GPTerminal.py
 # Create a symbolic link to move the script to a directory in your PATH
 sudo ln -s $(pwd)/GPTerminal.py /usr/local/bin/GPTerminal
 
-# Print the ASCII art with proper indentation
-echo "$(tput setaf 2)"
-echo ""
-echo "   ___   ___  _____                    _             _  "
-echo "  / _ \ / _ \/__   \___ _ __ _ __ ___ (_)_ __   __ _| | "
-echo " / /_\// /_)/  / /\/ _ \ '__| '_ \` _ \| | '_ \ / _\` | | "
-echo "/ /_\\ / ___/  / / |  __/ |  | | | | | | | | | | (_| | | "
-echo "\____/\/      \/   \___|_|  |_| |_| |_|_|_| |_|\__,_|_| "
-echo ""
-echo "$(tput sgr0)"
+# Print the ASCII art with proper indentation only if the terminal is wide enough
+if [ "$(tput cols)" -gt 55 ]; then
+    echo "$(tput setaf 2)"
+    echo ""
+    echo "   ___   ___  _____                    _             _  "
+    echo "  / _ \ / _ \/__   \___ _ __ _ __ ___ (_)_ __   __ _| | "
+    echo " / /_\// /_)/  / /\/ _ \ '__| '_ \` _ \| | '_ \ / _\` | | "
+    echo "/ /_\\ / ___/  / / |  __/ |  | | | | | | | | | | (_| | | "
+    echo "\____/\/      \/   \___|_|  |_| |_| |_|_|_| |_|\__,_|_| "
+    echo ""
+    echo "$(tput sgr0)"
+fi
 
 # Print the installation message
 echo -e "\033[32mGPTerminal has been installed.\033[0m"
