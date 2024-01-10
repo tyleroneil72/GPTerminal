39d3788be2a10afdccf6731ee97604bad5ce211c
Tyler O'Neil <tyleroneildev@gmail.com>
2024-01-09 21:18:44 -0500

    Edit comment

--- installation.sh
+++ installation.sh
@@ -4,5 +4,5 @@
 pip3 install -r requirements.txt
 # Make the script executable
 chmod +x GPTerminal.py
-# Move the script to a directory in your PATH
+# Create a symbolic link to move the script to a directory in your PATH
 sudo ln -s $(pwd)/GPTerminal.py /usr/local/bin/GPTerminal
